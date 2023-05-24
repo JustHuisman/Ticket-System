592ba745cc5a28088ba05693694dd01702a5ebdb
JustHuisman <justhuisman@live.nl>
2023-05-24 14:05:32 +0200

    updated users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name', 50);
+            $table->string('last_name', 100);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('telephonenumber');
+            $table->boolean('is_admin');
             $table->rememberToken();
             $table->timestamps();
         });
