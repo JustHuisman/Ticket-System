4db6396febb97ff60c0d348621e0e2838e90085a
JustHuisman <justhuisman@live.nl>
2023-05-25 00:11:32 +0200

    more seeders

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -12,6 +12,23 @@ class UserTableSeeder extends Seeder
      */
     public function run(): void
     {
+        DB::table('users')->insert([
+            'first_name'    => 'Just',
+            'last_name'     => 'Huisman',
+            'email'         => 'just@test.nl',
+            'password'      => bcrypt('Script123!'),
+            'is_admin'      => TRUE,
+            'created_at'    => now(),
+        ]);
+
+        DB::table('users')->insert([
+            'first_name'    => 'Non-Admin',
+            'last_name'     => 'User',
+            'email'         => 'nonadmin@test.nl',
+            'password'      => bcrypt('Script456!'),
+            'created_at'    => now(),
+        ]);
+        
         \App\Models\User::factory(50)->create();
     }
 }
