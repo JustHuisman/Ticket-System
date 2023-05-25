bf41264790d7c27b5f2aa82f820e6e9d49d0cdb2
JustHuisman <justhuisman@live.nl>
2023-05-25 23:00:33 +0200

    auth controller for JWT

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,4 +43,19 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
